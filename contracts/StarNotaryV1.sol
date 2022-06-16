6b4346004c23248efbe50e76888d3d0fcfc4c8ac
M Faried <m.a.faried@gmail.com>
2022-06-16 13:55:28 +0200

    The basic contract of the star notary system.

---
+++ contracts/StarNotaryV1.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.8.0;
+
+contract StarNotary {
+    string public starName;
+    address public starOwner;
+
+    event starClaimed(address owner);
+
+    constructor(){
+        starName = "Awesome Udacity Star";
+    }
+
+    function claimStar() public {
+        starOwner = msg.sender;
+        emit starClaimed(msg.sender);
+    }
+}
