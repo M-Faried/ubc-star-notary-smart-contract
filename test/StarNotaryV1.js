f95b9a922bcce9f7b893933e57f95cbb264695ff
M Faried <m.a.faried@gmail.com>
2022-06-16 14:51:12 +0200

    Creating more tests.

--- test/StarNotaryV1.js
+++ test/StarNotaryV1.js
@@ -1,6 +1,6 @@
 // import 'babel-polyfill';
-
-const StarNotaryV1 = require('StarNotaryV1.sol');
+// const StarNotaryV1 = require('../build/contracts/StarNotaryV1.json');
+const StarNotaryV1 = artifacts.require('StarNotaryV1');
 
 let accounts;
 let owner;
@@ -17,8 +17,23 @@ it('Has the correct name', async () => {
 })
 
 it('Can be claimed', async () => {
-    let instance = await StarNotaryV1.deplyed();
-    instance.claimStar({ from: owner });
-    let starOwner = instance.starOwner.call();
+    let instance = await StarNotaryV1.deployed();
+    await instance.claimStar({ from: owner });
+    let starOwner = await instance.starOwner.call();
     assert.equal(starOwner, owner);
+})
+
+it('Can change owners', async () => {
+    let instance = await StarNotaryV1.deployed();
+    let secondUser = accounts[1];
+
+    // Assigning star to the first user
+    await instance.claimStar({ from: owner });
+    let starOwner = await instance.starOwner.call();
+    assert.equal(starOwner, owner);
+
+    // Assigning star to the second user
+    await instance.claimStar({ from: secondUser });
+    let secondOwner = await instance.starOwner.call();
+    assert.equal(secondOwner, secondUser);
 })
